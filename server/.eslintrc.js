0de5c74195720c28bd183e69bba8b31963980c3e
Walkers15 <qortmd7777@gmail.com>
2023-01-09 12:51:54 +0900

    feature: 프로젝트 초기 세팅

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { node: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./server/tsconfig.json", sourceType: "module" },
+  plugins: ["@typescript-eslint/eslint-plugin", "unused-imports"],
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/member-ordering": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+    quotes: ["error", "double"],
+  },
+};
